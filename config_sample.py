6ea9934a9f15a0e55aa177429daa305034762fd0
Karl Benedict <kbene@unm.edu>
2020-12-10 16:12:52 -0700

    updated gitignore

---
+++ config_sample.py
@@ -0,0 +1,13 @@
+# create a file called secrets.py based on this template
+
+# Digital Commons
+dc_api_key = "something"
+dc_api_token = "something"
+
+# Dryad
+dryad_client_id = "something"
+dryad_secret = "something"
+
+# Datacite
+datacite_user = "something"
+datacite_pw = "something"
